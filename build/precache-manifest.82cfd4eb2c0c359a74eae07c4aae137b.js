c12aa75d02bd16c9f64b03a991c8d59d176c64f7
Jennifer He <gehly@simmons.edu>
2019-04-18 15:02:52 -0400

    Add files via upload

---
+++ build/precache-manifest.82cfd4eb2c0c359a74eae07c4aae137b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a634034cbf1b98a33641b2589e93d554",
+    "url": "/static/media/lilnemo.a634034c.png"
+  },
+  {
+    "revision": "a5f09250bc2b92abed48",
+    "url": "/static/css/main.0165dbbd.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d9c981a1df552789b33d64e2d42c053",
+    "url": "/static/media/ending.5d9c981a.png"
+  },
+  {
+    "revision": "33e2ec9c7b9ceb66ec8c",
+    "url": "/static/js/2.c8ab9f6c.chunk.js"
+  },
+  {
+    "revision": "06e51a1b228f388688b319a9d6f874d6",
+    "url": "/static/media/nemo.06e51a1b.png"
+  },
+  {
+    "revision": "cc29c91f81553aab2a91a1853812d595",
+    "url": "/static/media/therealnemo.cc29c91f.png"
+  },
+  {
+    "revision": "cf5bcb087b41ae9fb20d21115370a679",
+    "url": "/static/media/walkonwater.cf5bcb08.png"
+  },
+  {
+    "revision": "29070094578e025ace001b7d9584ec6b",
+    "url": "/static/media/waterworld.29070094.png"
+  },
+  {
+    "revision": "a5f09250bc2b92abed48",
+    "url": "/static/js/main.27f580aa.chunk.js"
+  },
+  {
+    "revision": "79b6d2cb5835b83195f93b3d271d6bf8",
+    "url": "/static/media/clownfriend.79b6d2cb.png"
+  },
+  {
+    "revision": "c71436fb9e2672a5e55ab74d82ed902d",
+    "url": "/static/media/running.c71436fb.png"
+  },
+  {
+    "revision": "d9377c931a3d7e5f9f86117813e6e9de",
+    "url": "/static/media/sunbackground.d9377c93.png"
+  },
+  {
+    "revision": "cd34087af437442185d6ca10cfa49a9a",
+    "url": "/static/media/background.cd34087a.png"
+  },
+  {
+    "revision": "aa49a28f44f4fbdca277d3062fa810a9",
+    "url": "/static/media/drawing.aa49a28f.png"
+  },
+  {
+    "revision": "46d7d0246182d7a14f8bfc7de9679a7a",
+    "url": "/static/media/go.46d7d024.png"
+  },
+  {
+    "revision": "33e2ec9c7b9ceb66ec8c",
+    "url": "/static/css/2.6f59c659.chunk.css"
+  },
+  {
+    "revision": "02b2a5398e160b63066dad888024071b",
+    "url": "/index.html"
+  }
+];
